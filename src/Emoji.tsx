c7daac5277829677ec6e27565d2acfc37033fe0d
Ian Felix <felix.ian@estudante.ufjf.br>
2025-01-06 20:24:05 -0300

    adiciona eventos

--- src/Emoji.tsx
+++ src/Emoji.tsx
@@ -1,5 +1,29 @@
-import "./Emoji.css"
+import "./Emoji.css";
+
+const EMOJIS = new Map<string, string>([
+  ["happy", "🙂"],
+  ["sick", "🤢"],
+  ["dead", "😵"],
+]);
 
 export default function Emoji() {
-    return <div className="emoji">🙂</div>
+  let situacao = "dead";
+
+    function toHappy(){
+        console.log("toHappy()!")
+    }
+
+    function toDead(){
+        console.log("toDead()!")
+    }
+
+  return (
+    <div className="emoji">
+      <div className="situacao">{EMOJIS.get(situacao) || "🫥"}</div>
+      <div className="acoes">
+        <button onClick={toHappy}>Vivo</button>
+        <button onClick={toDead}>Morto</button>
+      </div>
+    </div>
+  );
 }
